a79e80fea69ee774451911005a6a95c7abdd6fab
fusupo <fusupo@gmail.com>
2017-08-10 15:28:07 -0700

    creates basic two pane scroll view within calendar screen

--- screens/CalendarScreen.js
+++ screens/CalendarScreen.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { ScrollView, StyleSheet, View } from 'react-native';
+import {
+  ScrollView,
+  Component,
+  StyleSheet,
+  View,
+  Text,
+  PanResponder,
+  Animated,
+  Dimensions
+} from 'react-native';
 
 import OrgHabits from '../components/OrgHabits';
 import OrgAgenda from '../components/OrgAgenda';
@@ -11,56 +20,163 @@ const orgTimestampUtils = require('org-parse').OrgTimestamp;
 
 import { completeHabit, resetHabit } from '../actions';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 15
-  }
-});
+class Viewport extends React.Component {
+  constructor(props) {
+    super(props);
 
-class CalendarScreen extends React.Component {
-  static navigationOptions = () => ({
-    title: 'calendar'
-  });
+    this.state = {
+      stageW: 0,
+      stageH: 0,
+      prevTop: 0,
+      currTop: 0
+    };
+
+    this._previousTop = 82;
+    this.panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (e, gesture) => {
+        this.setState({ currTop: this.state.prevTop + gesture.dy });
+      },
+      onPanResponderRelease: (e, gesture) => {
+        let h = this.state.stageH - barHeight;
+        let fourth = h / 4;
+        let half = h / 2;
+        let flr = Math.floor(this.state.currTop / fourth);
+
+        let newState;
+        switch (flr) {
+          case 0:
+            newState = {
+              prevTop: 0,
+              currTop: 0
+            };
+            break;
+          case 1:
+          case 2:
+            newState = {
+              prevTop: half,
+              currTop: half
+            };
+            break;
+          case 3:
+            newState = {
+              prevTop: h,
+              currTop: h
+            };
+            break;
+        }
+        this.setState(newState);
+      }
+    });
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <ScrollView>
+      <View
+        style={styles.mainContainer}
+        onLayout={event => {
+          var { x, y, width, height } = event.nativeEvent.layout;
+          this.setState({ stageW: width, stageH: height });
+        }}>
+        <ScrollView
+          style={[
+            styles.viewA,
+            { height: this.state.currTop + barHeight / 2 }
+          ]}>
           <OrgAgenda />
         </ScrollView>
-        <ScrollView>
+        <ScrollView
+          style={[
+            styles.viewB,
+            {
+              height: this.state.stageH -
+                barHeight -
+                this.state.currTop +
+                barHeight / 2
+            }
+          ]}>
           <OrgHabits />
         </ScrollView>
+        {this.renderDraggable()}
+      </View>
+    );
+  }
+
+  renderDraggable() {
+    return (
+      <View style={styles.draggableContainer}>
+        <Animated.View
+          {...this.panResponder.panHandlers}
+          style={[
+            styles.bar,
+            {
+              left: 0,
+              top: this.state.currTop,
+              width: this.state.stageW
+            }
+          ]}>
+          <View style={styles.barInner}><Text>{'helllooooooo!'}</Text></View>
+        </Animated.View>
       </View>
     );
   }
 }
 
-// const CalendarScreen = ({ habits, habitData, onHabitPress }) => (
-// );
+const barHeight = 50;
+var styles = StyleSheet.create({
+  bar: {
+    height: barHeight,
+    backgroundColor: '#ccc5'
+  },
+  barInner: {
+    backgroundColor: '#333',
+    position: 'absolute',
+    top: barHeight / 2 - 5,
+    height: 10,
+    width: '100%'
+  },
+  mainContainer: {
+    flex: 1
+  },
+  draggableContainer: {
+    position: 'absolute'
+  },
+  viewA: {
+    width: '100%',
+    height: 20
+  },
+  viewB: {
+    width: '100%',
+    height: 20
+  }
+});
 
+//module.exports = PanResponderExample;
+class CalendarScreen extends React.Component {
+  static navigationOptions = () => ({
+    title: 'calendar'
+  });
+  render() {
+    return <Viewport />;
+  }
+}
+
+// <View style={styles.container}>
+// <ScrollView>
+// <OrgAgenda />
+// </ScrollView>
+// <ScrollView>
+// <OrgHabits />
+// </ScrollView>
+// </View>
 ////////////////////////////////////////////////////////////////////////////////
 
 const mapStateToProps = state => {
-  // const nodes = Object.values(state.orgBuffers).reduce(
-  //   (m, v) => m.concat(Object.values(v.orgNodes)),
-  //   []
-  // );
-
-  return {
-    // habits: [],
-    // habitData: []
-  };
+  return {};
 };
 
 const mapDispatchToProps = dispatch => {
-  return {
-    // onHabitPress: nodeID => {
-    //   const nowStr = orgTimestampUtils.serialize(orgTimestampUtils.now());
-    //   dispatch(completeHabit(nodeID, nowStr));
-    //   dispatch(resetHabit(nodeID, nowStr));
-    // }
-  };
+  return {};
 };
 
 export default connect(mapStateToProps, mapDispatchToProps)(CalendarScreen);
