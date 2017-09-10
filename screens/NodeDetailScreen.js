8ccbd5958d42999fff69fd226cfecce91d46c2e9
fusupo <fusupo@gmail.com>
2017-09-09 22:20:48 -0700

    adds childlist lock on nodedetails screen

--- screens/NodeDetailScreen.js
+++ screens/NodeDetailScreen.js
@@ -21,8 +21,11 @@ import {
   ScrollView,
   StyleSheet,
   Text,
+  TouchableHighlight,
   View
 } from 'react-native';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 import OrgHeadlineEditable from '../components/OrgHeadlineEditable';
 import OrgScheduling from '../components/OrgScheduling';
@@ -48,117 +51,152 @@ const styles = StyleSheet.create({
   }
 });
 
-const NodeDetailScreen = ({
-  tree,
-  bufferID,
-  nodeID,
-  node,
-  onPressDeleteNode,
-  onAddProp,
-  onUpdateProp,
-  onRemoveProp,
-  onAddLogNote,
-  onUpdateLogNote,
-  onRemoveLogNote,
-  onUpdateNodeBody,
-  onAddOnePress,
-  isNew
-}) => {
-  if (node) {
-    // childNodes
-    const children = OrgTreeUtil.findBranch(tree, nodeID).children;
-    let list = null;
-    if (children.length > 0) {
-      list = (
-        <ScrollView style={{ flex: 1, margin: 10, marginBottom: 40 }}>
-          <OrgList data={children} bufferID={bufferID} isLocked={true} />
-        </ScrollView>
-      );
-    }
-
-    return (
-      <View style={styles.container}>
-        <SplitPane
-          viewA={
-            <ScrollView style={styles.container}>
-              <View
-                style={[styles.container, styles.border, { marginTop: 20 }]}>
-                <OrgHeadlineEditable
-                  bufferID={bufferID}
-                  nodeID={nodeID}
-                  autoFocus={isNew}
-                />
-              </View>
-              <View style={[styles.container, styles.border]}>
-                <OrgScheduling
-                  bufferID={bufferID}
-                  nodeID={nodeID}
-                  isCollapsed={true}
-                />
-              </View>
-              <View style={[styles.container, styles.border]}>
-                <OrgDrawer
-                  drawer={node.propDrawer}
-                  isCollapsed={true}
-                  onAddProp={onAddProp(bufferID, nodeID)}
-                  onUpdateProp={onUpdateProp(bufferID, nodeID)}
-                  onRemoveProp={onRemoveProp(bufferID, nodeID)}
-                />
-              </View>
-              <View style={[styles.container, styles.border]}>
-                <OrgLogbook
-                  log={node.logbook}
-                  isCollapsed={true}
-                  onAddLogNote={onAddLogNote(bufferID, nodeID)}
-                  onUpdateLogNote={onUpdateLogNote(bufferID, nodeID)}
-                  onRemoveLogNote={onRemoveLogNote(bufferID, nodeID)}
-                />
-              </View>
-              <View style={[styles.container, styles.border]}>
-                <OrgBody
-                  onUpdateNodeBody={onUpdateNodeBody(bufferID, nodeID)}
-                  bodyText={node.body}
-                />
-              </View>
-            </ScrollView>
-          }
-          viewB={
-            <View style={{ flex: 1 }}>
-              <Button
-                onPress={() => {
-                  onAddOnePress(bufferID, nodeID, node);
-                }}
-                title="Add Child"
-                color="#841584"
-                accessibilityLabel="add child node"
-              />
-              {list}
-            </View>
-          }
-        />
-        <Button
-          onPress={() => {
-            ActionSheetIOS.showActionSheetWithOptions(
-              {
-                options: ['confirm', 'cancel']
-              },
-              idx => {
-                if (idx === 0) {
-                  onPressDeleteNode(bufferID, nodeID);
-                }
-              }
-            );
-          }}
-          title="DeleteNode"
-          color="#841584"
-          accessibilityLabel="delete this node"
-        />
-      </View>
-    );
-  } else {
-    return <View><Text>{'node is gone '}</Text></View>;
+class NodeDetailScreen extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      childListIsLocked: true
+    };
   }
-};
+  render() {
+    const {
+      tree,
+      bufferID,
+      nodeID,
+      node,
+      onPressDeleteNode,
+      onAddProp,
+      onUpdateProp,
+      onRemoveProp,
+      onAddLogNote,
+      onUpdateLogNote,
+      onRemoveLogNote,
+      onUpdateNodeBody,
+      onAddOnePress,
+      isNew
+    } = this.props;
+    if (node) {
+      // childNodes
+      const children = OrgTreeUtil.findBranch(tree, nodeID).children;
+      let list = null;
+      if (children.length > 0) {
+        list = (
+          <ScrollView style={{ flex: 1, margin: 10, marginBottom: 40 }}>
+            <OrgList
+              data={children}
+              bufferID={bufferID}
+              isLocked={this.state.childListIsLocked}
+            />
+          </ScrollView>
+        );
+      }
+
+      return (
+        <View style={styles.container}>
+          <SplitPane
+            viewA={
+              <ScrollView style={styles.container}>
+                <View
+                  style={[styles.container, styles.border, { marginTop: 20 }]}>
+                  <OrgHeadlineEditable
+                    bufferID={bufferID}
+                    nodeID={nodeID}
+                    autoFocus={isNew}
+                  />
+                </View>
+                <View style={[styles.container, styles.border]}>
+                  <OrgScheduling
+                    bufferID={bufferID}
+                    nodeID={nodeID}
+                    isCollapsed={true}
+                  />
+                </View>
+                <View style={[styles.container, styles.border]}>
+                  <OrgDrawer
+                    drawer={node.propDrawer}
+                    isCollapsed={true}
+                    onAddProp={onAddProp(bufferID, nodeID)}
+                    onUpdateProp={onUpdateProp(bufferID, nodeID)}
+                    onRemoveProp={onRemoveProp(bufferID, nodeID)}
+                  />
+                </View>
+                <View style={[styles.container, styles.border]}>
+                  <OrgLogbook
+                    log={node.logbook}
+                    isCollapsed={true}
+                    onAddLogNote={onAddLogNote(bufferID, nodeID)}
+                    onUpdateLogNote={onUpdateLogNote(bufferID, nodeID)}
+                    onRemoveLogNote={onRemoveLogNote(bufferID, nodeID)}
+                  />
+                </View>
+                <View style={[styles.container, styles.border]}>
+                  <OrgBody
+                    onUpdateNodeBody={onUpdateNodeBody(bufferID, nodeID)}
+                    bodyText={node.body}
+                  />
+                </View>
+              </ScrollView>
+            }
+            viewB={
+              <View style={{ flex: 1 }}>
+                <TouchableHighlight
+                  onPress={() => {
+                    this.setState({
+                      childListIsLocked: !this.state.childListIsLocked
+                    });
+                  }}>
+                  <View
+                    style={{
+                      flexDirection: 'row',
+                      backgroundColor: '#cccccc'
+                    }}>
+                    <Ionicons
+                      style={{ flex: 1 }}
+                      name={
+                        this.state.childListIsLocked ? 'md-lock' : 'md-unlock'
+                      }
+                      size={20}
+                      style={{ marginLeft: 5 }}
+                    />
+                    {!this.state.childListIsLocked &&
+                      <Button
+                        onPress={() => {
+                          onAddOnePress(bufferID, nodeID, node);
+                        }}
+                        title="Add Child"
+                        color="#841584"
+                        accessibilityLabel="add child node"
+                      />}
+                  </View>
+                </TouchableHighlight>
+                {list}
+              </View>
+            }
+          />
+          <Button
+            onPress={() => {
+              ActionSheetIOS.showActionSheetWithOptions(
+                {
+                  options: ['confirm', 'cancel']
+                },
+                idx => {
+                  if (idx === 0) {
+                    onPressDeleteNode(bufferID, nodeID);
+                  }
+                }
+              );
+            }}
+            title="DeleteNode"
+            color="#841584"
+            accessibilityLabel="delete this node"
+          />
+        </View>
+      );
+    } else {
+      return <View><Text>{'node is gone '}</Text></View>;
+    }
+  }
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
