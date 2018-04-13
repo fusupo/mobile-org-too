7aa8b5266bfb84bb586b2450555e0080d5d5a641
fusupo <fusupo@gmail.com>
2018-04-12 17:20:53 -0700

    removes dead code

--- main.js
+++ main.js
@@ -8,7 +8,6 @@ import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
 
 import DropboxDataSource from './utilities/DropboxDataSource.js';
-// import mobileOrgTooApp from './reducers';
 import mobileOrgTooApp from './reducers/main';
 
 import StacksOverTabs from './navigation/StacksOverTabs';
